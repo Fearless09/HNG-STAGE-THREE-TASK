0f6ad82e2062b555c6cdd27e8adf3cd622f67184
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-09-20 18:57:37 +0100

    Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='p-4 bg-blue-950 text-white'>
-            <div className='container mx-auto mt-5'>
+        <footer className='p-4 bg-blue-950 text-white mt-7'>
+            <div className='container mx-auto'>
                 <p className='text-center text-xl font-normal'>&copy; Opeyemi_dev 2023. Designed and Developed by Opeyemi_dev</p>
             </div>
         </footer>
--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -16,25 +16,27 @@ function SignUp() {
     }
 
     return (
-        <div className='max-w-[450px] mx-auto p-4 '>
-            <h1 className='text-center my-5 uppercase font-semibold text-3xl'>Create an Account</h1>
-            <form action="" onSubmit={signUp}>
-                <div className='flex flex-col gap-2'>
-                    <label htmlFor="email" className='ms-1 text-xl'>Email:</label>
-                    <input type="email" name="email" id="email" placeholder='Enter your Email' onChange={e => setEmail(e.target.value)} className='border-2 rounded-md p-2 border-blue-300' />
-                </div>
-                <div className='flex flex-col gap-2 mt-5'>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name='password' id='password' placeholder='Enter your password' onChange={e => setPaasword(e.target.value)} className='border-2 rounded-md p-2 border-blue-300' />
-                </div>
+        <>
+            <div className='max-w-[450px] mx-auto p-4 '>
+                <h1 className='text-center my-5 uppercase font-semibold text-3xl'>Create an Account</h1>
+                <form action="" onSubmit={signUp}>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor="email" className='ms-1 text-xl'>Email:</label>
+                        <input type="email" name="email" id="email" placeholder='Enter your Email' onChange={e => setEmail(e.target.value)} className='border-2 rounded-md p-2 border-blue-300' />
+                    </div>
+                    <div className='flex flex-col gap-2 mt-5'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' id='password' placeholder='Enter your password' onChange={e => setPaasword(e.target.value)} className='border-2 rounded-md p-2 border-blue-300' />
+                    </div>
 
-                <button type='submit' className='w-full bg-blue-800 text-white font-normal text-2xl py-2 px-1 mb-2 rounded-md mt-7 active:scale-[0.99] active:bg-blue-900'>RESIGTER</button>
-            </form>
+                    <button type='submit' className='w-full bg-blue-800 text-white font-normal text-2xl py-2 px-1 mb-2 rounded-md mt-7 active:scale-[0.99] active:bg-blue-900'>RESIGTER</button>
+                </form>
 
-            <Link to={'/signin'}>Have an accout? Click Here to Login</Link>
+                <Link to={'/signin'}>Have an accout? Click Here to Login</Link>
 
+            </div>
             <Footer />
-        </div>
+        </>
     )
 }
 
